5fa775cb2d1a6acc984021079e491e8cd6d051d6
katosan03 <221205050@ie.meijo-u.ac.jp>
2023-09-05 13:26:49 +0900

    演習7までの変更，editTextの作成

--- app/src/main/java/jp/co/meijou/android/s221205050/MainActivity.java
+++ app/src/main/java/jp/co/meijou/android/s221205050/MainActivity.java
@@ -2,13 +2,46 @@ package jp.co.meijou.android.s221205050;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.co.meijou.android.s221205050.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    private int textRes = R.string.my_name;
+    private int imageRes = R.drawable.baseline_battery_1_bar_24;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view ->{
+            var text = binding.editText.getText().toString();
+            binding.text.setText(text);
+        });
+
+        binding.editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //テキストが更新される直前に呼ばれる
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //文字を1つ入力された時に呼ばれる
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //テキストが更新した後に呼ばれる
+                binding.text.setText(s.toString());
+            }
+        });
+
     }
 }
