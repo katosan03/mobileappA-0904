333e6221c1c66f9552fcaa5644f1faf29b326c93
katosan03 <221205050@ie.meijo-u.ac.jp>
2023-09-06 16:28:01 +0900

    （演習3）Activity間のデータ双方向の受け渡し

--- app/src/main/java/jp/co/meijou/android/s221205050/MainActivity2.java
+++ app/src/main/java/jp/co/meijou/android/s221205050/MainActivity2.java
@@ -2,17 +2,41 @@ package jp.co.meijou.android.s221205050;
 
 //----NOTICE!!--------授業ではMainActivity3------
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import java.util.Optional;
+
 import jp.co.meijou.android.s221205050.databinding.ActivityMain2Binding;
 import jp.co.meijou.android.s221205050.databinding.ActivityMain3Binding;
 
 public class MainActivity2 extends AppCompatActivity {
     private ActivityMain2Binding binding;
+
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()){
+                    case RESULT_OK:
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.textResult.setText(text));
+                        break;
+                    case RESULT_CANCELED:
+                        binding.textResult.setText("Result: Canceled");
+                        break;
+                    default:
+                        binding.textResult.setText("Result: Unknown(" + result.getResultCode() + ")");
+                        break;
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +62,12 @@ public class MainActivity2 extends AppCompatActivity {
             var intent = new Intent(this,SubActivity.class);
             intent.putExtra("text",binding.editTextName.getText().toString());
             startActivity(intent);
+        });
+
+        //取得
+        binding.buttonKidou.setOnClickListener(view ->{
+            var intent = new Intent(this, SubActivity.class);
+            getActivityResult.launch(intent);
         });
     }
 }
--- app/src/main/java/jp/co/meijou/android/s221205050/SubActivity.java
+++ app/src/main/java/jp/co/meijou/android/s221205050/SubActivity.java
@@ -2,6 +2,7 @@ package jp.co.meijou.android.s221205050;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.Optional;
@@ -19,6 +20,20 @@ public class SubActivity extends AppCompatActivity {
 
         Optional.ofNullable(getIntent().getStringExtra("text"))
                 .ifPresent(text -> binding.textView.setText(text));
+
+
+        binding.buttonOk.setOnClickListener(view ->{
+            var intent = new Intent();
+            intent.putExtra("ret", "meijo");
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        binding.buttonCancel.setOnClickListener(view ->{
+            setResult(RESULT_CANCELED);
+            finish();
+        });
+
     }
 
 }
